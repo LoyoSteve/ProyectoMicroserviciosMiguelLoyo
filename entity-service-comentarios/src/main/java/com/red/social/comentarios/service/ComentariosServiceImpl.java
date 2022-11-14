1164e2ed73e32fde1cc5c895a1121b7ff553891b
Miguel Loyo <ing.mendez999@gmail.com>
2022-11-13 20:46:24 -0600

    proyecto terminado

--- entity-service-comentarios/src/main/java/com/red/social/comentarios/service/ComentariosServiceImpl.java
+++ entity-service-comentarios/src/main/java/com/red/social/comentarios/service/ComentariosServiceImpl.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 import com.red.social.comentarios.entity.Comentario;
 import com.red.social.comentarios.modelo.ComentariosDto;
 import com.red.social.comentarios.repository.ComentariosRepository;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import com.red.social.comentarios.RabbitConfigConsumer;
 
 @Service
 public class ComentariosServiceImpl implements ComentariosService{
@@ -18,18 +20,21 @@ public class ComentariosServiceImpl implements ComentariosService{
 	}
 	
 	@Override
+	@RabbitListener(queues = RabbitConfigConsumer.QUEUE_COMENTARIOS)
 	public Comentario obtenerById(String id) {
 		return comentariosRepository.findById(id)
 				.orElseThrow(() -> ComentarioNoEncontradoException.from("No se encontro el comentario", id));
 	}
 
 	@Override
+	@RabbitListener(queues = RabbitConfigConsumer.QUEUE_COMENTARIOS)
 	public Comentario guardar(ComentariosDto comentarioDto) {
 		Comentario comentario = Comentario.from(comentarioDto);
 		return comentariosRepository.save(comentario);
 	}
 
 	@Override
+	@RabbitListener(queues = RabbitConfigConsumer.QUEUE_COMENTARIOS)
 	public Comentario modificar(String id, ComentariosDto comentarioDto) {
 		Comentario comentario = comentariosRepository.findById(id)
 				.orElseThrow(() -> ComentarioNoEncontradoException.from("No se encontro el comentario", id));
@@ -42,12 +47,14 @@ public class ComentariosServiceImpl implements ComentariosService{
 	}
 
 	@Override
+	@RabbitListener(queues = RabbitConfigConsumer.QUEUE_COMENTARIOS)
 	public List<Comentario> obtenerTodo() {
 
 		return comentariosRepository.findAll();
 	}
 
 	@Override
+	@RabbitListener(queues = RabbitConfigConsumer.QUEUE_COMENTARIOS)
 	public void remover(String id) {
 		comentariosRepository.findById(id)
 				.orElseThrow(() -> ComentarioNoEncontradoException.from("No se encontro el comentario", id));
--- entity-service-publicaciones/src/main/java/com/red/social/publicaciones/service/PublicacionesServiceImpl.java
+++ entity-service-publicaciones/src/main/java/com/red/social/publicaciones/service/PublicacionesServiceImpl.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 import com.red.social.publicaciones.entity.Publicacion;
 import com.red.social.publicaciones.modelo.PublicacionDto;
 import com.red.social.publicaciones.repository.PublicacionesRepository;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import com.red.social.publicaciones.RabbitConfigConsumer;
 
 @Service
 public class PublicacionesServiceImpl implements PublicacionesService{
@@ -18,18 +20,21 @@ public class PublicacionesServiceImpl implements PublicacionesService{
 	}
 	
 	@Override
+	@RabbitListener(queues = RabbitConfigConsumer.QUEUE_PUBLICACIONES)
 	public Publicacion obtenerById(String id) {
 		return publicacionesRepository.findById(id)
 				.orElseThrow(() -> PublicacionNoEncontradoException.from("No se encontro publicacion", id));
 	}
 
 	@Override
+	@RabbitListener(queues = RabbitConfigConsumer.QUEUE_PUBLICACIONES)
 	public Publicacion guardar(PublicacionDto publicacionDto) {
 		Publicacion publucacion = Publicacion.from(publicacionDto);
 		return publicacionesRepository.save(publucacion);
 	}
 
 	@Override
+	@RabbitListener(queues = RabbitConfigConsumer.QUEUE_PUBLICACIONES)
 	public Publicacion modificar(String id, PublicacionDto publicacionDto) {
 		Publicacion publicacion = publicacionesRepository.findById(id)
 				.orElseThrow(() -> PublicacionNoEncontradoException.from("No se encontro publicacion", id));
@@ -42,12 +47,14 @@ public class PublicacionesServiceImpl implements PublicacionesService{
 	}
 
 	@Override
+	@RabbitListener(queues = RabbitConfigConsumer.QUEUE_PUBLICACIONES)
 	public List<Publicacion> obtenerTodo() {
 
 		return publicacionesRepository.findAll();
 	}
 
 	@Override
+	@RabbitListener(queues = RabbitConfigConsumer.QUEUE_PUBLICACIONES)
 	public void remover(String id) {
 		publicacionesRepository.findById(id)
 				.orElseThrow(() -> PublicacionNoEncontradoException.from("No se encontro publicacion", id));
